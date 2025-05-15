26c756ff9cea494e5f964ca2f9996b38212c066c
Laibakhan123 <24054992@student.hhs.nl>
2025-05-15 11:13:33 +0200

    gedrag aangepast

--- src/main/java/com/example/opt3strategymethod/Monster.java
+++ src/main/java/com/example/opt3strategymethod/Monster.java
@@ -8,7 +8,7 @@ public class Monster {
     }
 
     public void versperWeg() {
-        gedrag.reageer(); // Strategy Pattern: gedraggggg via interface
+        gedrag.reageer(); // Strategy Pattern: gedragee via interface
     }
 
     public String getNaam() {
